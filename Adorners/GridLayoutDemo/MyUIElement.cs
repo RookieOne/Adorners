7a744d139252361ad2bf60498e6a5ba28ec9af45
Jonathan <rookieone@gmail.com>
2009-11-02 18:31:55 -0600

    Initial Commit

---
+++ Adorners/GridLayoutDemo/MyUIElement.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace GridLayoutDemo
+{
+    public class MyUIElement : UIElement
+    {
+        protected override Size MeasureCore(Size availableSize)
+        {
+            var desiredSize = DesiredSize;
+            var s = availableSize;
+            //return base.MeasureCore(availableSize);
+            return new Size(100, 100);
+        }
+
+        protected override void ArrangeCore(Rect finalRect)
+        {
+            var desiredSize = DesiredSize;
+            var r = finalRect;
+            base.ArrangeCore(finalRect);
+        }
+    }
+}
---
+++ Adorners/IsRequiredDemo/IsRequiredAdorner.cs
@@ -0,0 +1,52 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+namespace IsRequiredDemo
+{
+    public class IsRequiredAdorner : Adorner
+    {
+        public IsRequiredAdorner(UIElement adornedElement)
+            : base(adornedElement)
+        {
+            VisualChildren = new VisualCollection(this);
+
+            Control = new Control();
+            VisualChildren.Add(Control);
+
+            Style style = FindResource("IsRequiredAdornerStyle") as Style;
+
+            if (style != null)
+                Control.Style = style;
+        }
+
+        VisualCollection VisualChildren { get; set; }
+
+        protected override int VisualChildrenCount
+        {
+            get { return VisualChildren.Count; }
+        }
+
+        Control Control { get; set; }
+
+        protected override Visual GetVisualChild(int index)
+        {
+            return VisualChildren[index];
+        }
+
+        protected override Size MeasureOverride(Size constraint)
+        {
+            Control.Measure(constraint);
+
+            return base.MeasureOverride(constraint);
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            Control.Arrange(new Rect(finalSize));
+
+            return base.ArrangeOverride(finalSize);
+        }
+    }
+}
